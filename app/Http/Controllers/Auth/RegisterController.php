4b259cb15553dec9dd2fba03a0860264e2d25593
farhaz17 <farhaz171717@gmail.com>
2020-05-07 15:22:48 +0530

    env updated

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -65,13 +65,13 @@ class RegisterController extends Controller
             $mail->isSMTP(); // Set mailer to use SMTP
             $mail->Host = 'smtp.gmail.com'; // Specify main and backup SMTP servers
             $mail->SMTPAuth = true;// Enable SMTP authentication
-            $mail->Username = 'farhaz@specbee.com'; // SMTP username
+            $mail->Username = env("MAIL_USERNAME"); // SMTP username
             $mail->Password = env("MAIL_PASSWORD"); // SMTP password
             $mail->SMTPSecure = 'tls'; // Enable TLS encryption, `ssl` also accepted
             $mail->Port = 587; // TCP port to connect to
 
             //Recipients
-            $mail->setFrom('farhaz@specbee.com', 'Mailer');
+            $mail->setFrom(env("MAIL_FROM_ADDRESS"), 'Mailer');
             $mail->addAddress($request->email, $request->username);	// Add a recipient, Name is optional
 
             //Content
